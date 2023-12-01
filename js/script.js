930383e86bb2627bfd2b5a2fbebfc31e18a9d65a
Anna Powaska <annapowaska@op.pl>
2023-12-01 09:37:42 +0100

    Taskrunner updated, plugin added

--- js/script.js
+++ js/script.js
@@ -3,13 +3,13 @@
 function titleClickHandler(event){
   console.log('Link was clicked!');
 
- /*[DONE] remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
- const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
 
   /* [DONE] add class 'active' to the clicked link */
   event.preventDefault();
@@ -19,12 +19,12 @@ for(let activeLink of activeLinks){
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('article.active');
 
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
-}
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
   /* get 'href' attribute from the clicked link */
-  const articleSelector = clickedElement.getAttribute("href");
+  const articleSelector = clickedElement.getAttribute('href');
 
 
   /* find the correct article using the selector (value of 'href' attribute) */
@@ -32,9 +32,9 @@ for(let activeArticle of activeArticles){
 
 
   /* add class 'active' to the correct article */
-  targetArticle.classList.add("active");
+  targetArticle.classList.add('active');
 
-console.log(event);
+  console.log(event);
 }
 
 const optArticleSelector = '.post',
@@ -55,7 +55,7 @@ function generateTitleLinks(){
   for (let article of articles) {
     /* get the article id */
 
-  const articleId = article.getAttribute('id'); //I think I can also do: const articleId = optArticleSelector.getAttribute ('id') ??
+    const articleId = article.getAttribute('id'); //I think I can also do: const articleId = optArticleSelector.getAttribute ('id') ??
 
     /* find the title element */
 
